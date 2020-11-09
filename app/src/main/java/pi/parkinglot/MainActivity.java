795876c0e3b79f27678ba3abcc5330e5336c8ef6
Calslock <admin@calslock.net>
2020-11-09 17:32:41 +0100

    snapshot as of b5, 17:31

--- app/src/main/java/pi/parkinglot/MainActivity.java
+++ app/src/main/java/pi/parkinglot/MainActivity.java
@@ -1,8 +1,12 @@
 package pi.parkinglot;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Parkinglot b5");
+    }
+
+    public void goToRegister(View view) {
+        Intent intent = new Intent(this, Register.class);
+        EditText loginbox = (EditText) findViewById(R.id.loginEmail);
+        EditText passwordbox = (EditText) findViewById(R.id.loginPassword);
+        @Nullable
+        String login = loginbox.toString();
+        @Nullable
+        String password = passwordbox.toString();
+        intent.putExtra("login", login);
+        intent.putExtra("password", password);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/pi/parkinglot/Register.java
@@ -0,0 +1,19 @@
+package pi.parkinglot;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setTitle("Rejestracja");
+    }
+
+}
